3d19cb80c22fd11385939b17b0583c3945c1d4b0
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-21 21:56:04 +0200

    lesson 4

--- src/com/foretruff/HelloWorld.groovy
+++ src/com/foretruff/HelloWorld.groovy
@@ -1,10 +1,20 @@
 package com.foretruff
 
+import java.sql.DriverManager
+
 class HelloWorld {
+
     Integer value
+
 //  PackageScope
     static void main(String[] args) {
+        String value = "Hello world!"
+        Class<String> clazz = value.getClass();
+        assert clazz == String.class
         println "Hello world!"
+        println clazz.getClassLoader()
+        println HelloWorld.getClassLoader()
+        println DriverManager.getClassLoader()
     }
 
 }
