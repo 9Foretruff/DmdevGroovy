c3578f94fa8f7a754adeb2b80a7bb0cf474799bb
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-23 01:20:06 +0200

    lesson 15

---
+++ src/com/foretruff/lesson15/RangeRunner.groovy
@@ -0,0 +1,23 @@
+package com.foretruff.lesson15
+
+import static com.foretruff.lesson15.WeekDay.FR
+import static com.foretruff.lesson15.WeekDay.MO
+
+class RangeRunner {
+
+    static void main(String[] args) {
+        def range = 2..<8 // 2..9
+        assert range.getClass() == IntRange
+
+        assert range.get(3) == 5
+        assert !range.contains(8)
+
+        range.each { println it }
+
+        ('a'..'d').each { println it }
+        (MO..FR).each { println it }
+        (FR..MO).each { println it }
+
+    }
+
+}
---
+++ src/com/foretruff/lesson15/WeekDay.groovy
@@ -0,0 +1,5 @@
+package com.foretruff.lesson15
+
+enum WeekDay {
+    MO , TU , WE , TH , FR , SA ,SU
+}
