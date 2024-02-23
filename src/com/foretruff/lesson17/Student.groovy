772e4c6d45d35711d058d7b529b3dd2d975e0101
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-24 00:31:33 +0200

    lesson 20

--- src/com/foretruff/lesson17/Student.groovy
+++ src/com/foretruff/lesson17/Student.groovy
@@ -1,17 +1,34 @@
 package com.foretruff.lesson17
 
+import groovy.transform.Canonical
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.Immutable
 import groovy.transform.ToString
 import groovy.transform.TupleConstructor
+import groovy.transform.builder.Builder
+import groovy.util.logging.Slf4j
 
 @ToString
 @TupleConstructor
-class Student implements WIthId{
+@EqualsAndHashCode
+//@Canonical == all 3
+@Immutable
+@Builder
+//@Slf4j
+class Student implements WIthId {
     String firstName
     String lastName
     Integer age
 
-    def getAt(Integer index){
+    def getAt(Integer index) {
         index == 0 ? firstName : lastName
+    }
+
+    static void main(String[] args) {
+//        Student.builder()
+//                .firstName("Maks")
+//                .lastName("Rokitko")
+//                .build()
     }
 
 }
